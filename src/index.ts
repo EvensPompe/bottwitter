9befe482c16021dff6bc9e30090ecb5747b76d49
Evens Pompe <evenspompe9593@gmail.com>
2020-05-16 19:58:27 +0200

    install puppeteer goto twitter et login du bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hee');
+import dotenv from "dotenv";
+import puppeteer from "puppeteer";
+dotenv.config();
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://twitter.com/login');
+  await page.type("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > form > div > div:nth-child(6) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1udh08x > div > input", process.env.EMAIL_BOT);
+  await page.type("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > form > div > div:nth-child(7) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1udh08x > div > input", process.env.MDP_BOT);
+  await page.click("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > form > div > div:nth-child(8) > div");
+
+//  await browser.close();
+})();
